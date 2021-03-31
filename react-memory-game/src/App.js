0a59cac772fa3229dc232fafc981f4db260d76fd
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2021-03-31 15:18:35 +0200

    asd

--- react-memory-game/src/App.js
+++ react-memory-game/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 import Rocky from './photos/Rocky.png';
-import Chase from './photos/Chase.png';
-import Marshall from './photos/Marshall.png';
+import Chase_2 from './photos/Chase_2.png';
+import Marshall_2 from './photos/Marshall_2.png';
 import Rubble from './photos/Rubble.png';
-import Skye from './photos/Skye.png';
-import Zuma from './photos/Zuma.png';
+import Skye_2 from './photos/Skye_2.png';
+import Zuma_2 from './photos/Zuma_2.png';
 
 
 import CardBoard from "./Components/CardBoard";
@@ -15,17 +15,17 @@ function App() {
 
   const initialCards = [
     { id: 1, photo: Rocky, flipped: false },
-    { id: 2, photo: Chase, flipped: false },
-    { id: 3, photo: Marshall, flipped: false },
+    { id: 2, photo: Chase_2, flipped: false },
+    { id: 3, photo: Marshall_2, flipped: false },
     { id: 4, photo: Rubble, flipped: false },
-    { id: 5, photo: Skye, flipped: false },
-    { id: 6, photo: Zuma, flipped: false },
+    { id: 5, photo: Skye_2, flipped: false },
+    { id: 6, photo: Zuma_2, flipped: false },
     { id: 7, photo: Rocky, flipped: false },
-    { id: 8, photo: Chase, flipped: false },
-    { id: 9, photo: Marshall, flipped: false },
+    { id: 8, photo: Chase_2, flipped: false },
+    { id: 9, photo: Marshall_2, flipped: false },
     { id: 10, photo: Rubble, flipped: false },
-    { id: 11, photo: Skye, flipped: false },
-    { id: 12, photo: Zuma, flipped: false },
+    { id: 11, photo: Skye_2, flipped: false },
+    { id: 12, photo: Zuma_2, flipped: false },
   ];
 
   
