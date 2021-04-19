b608bcd68d01e3360d5fbae33829bca5ac68224b
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2021-04-20 01:32:44 +0200

    1

--- src/App.js
+++ src/App.js
@@ -19,17 +19,17 @@ import "./App.css";
 function App() {
   const initialCards = [
     { id: 1, photo: Rocky, flipped: false },
-    // { id: 2, photo: Chase, flipped: false },
-    // { id: 3, photo: Marshall, flipped: false },
-    // { id: 4, photo: Rubble, flipped: false },
-    // { id: 5, photo: Skye, flipped: false },
-    // { id: 6, photo: Zuma, flipped: false },
+    { id: 2, photo: Chase, flipped: false },
+    { id: 3, photo: Marshall, flipped: false },
+    { id: 4, photo: Rubble, flipped: false },
+    { id: 5, photo: Skye, flipped: false },
+    { id: 6, photo: Zuma, flipped: false },
     { id: 7, photo: Rocky, flipped: false },
-    // { id: 8, photo: Chase, flipped: false },
-    // { id: 9, photo: Marshall, flipped: false },
-    // { id: 10, photo: Rubble, flipped: false },
-    // { id: 11, photo: Skye, flipped: false },
-    // { id: 12, photo: Zuma, flipped: false },
+    { id: 8, photo: Chase, flipped: false },
+    { id: 9, photo: Marshall, flipped: false },
+    { id: 10, photo: Rubble, flipped: false },
+    { id: 11, photo: Skye, flipped: false },
+    { id: 12, photo: Zuma, flipped: false },
   ];
 
   const [cards, setCards] = useState(
