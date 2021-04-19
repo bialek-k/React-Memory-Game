6b1555fcf7690b6c6e2a9818170c9fea59f4ce3a
bialek-k <krzysztof.bialek1@gmail.com>
2021-04-19 13:24:12 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,107 +1,111 @@
-  import React, { useState } from 'react';
+import React, { useState } from "react";
 
-  // Import photos
-  import Rocky from './photos/Rocky.png';
-  import Chase from './photos/Chase.png';
-  import Marshall from './photos/Marshall.png';
-  import Rubble from './photos/Rubble.png';
-  import Skye from './photos/Skye.png';
-  import Zuma from './photos/Zuma.png';
+// Import photos
+import Rocky from "./photos/Rocky.png";
+import Chase from "./photos/Chase.png";
+import Marshall from "./photos/Marshall.png";
+import Rubble from "./photos/Rubble.png";
+import Skye from "./photos/Skye.png";
+import Zuma from "./photos/Zuma.png";
 
-  // Import components
-  import CardBoard from "./Components/CardBoard/CardBoard";
-  import Player from "./Components/Player/Player"
-  import NewPlayer from "./Components/NewPlayer/NewPlayer";
-  import EndModal from "./Components/EndModal/EndModal";
+// Import components
+import CardBoard from "./Components/CardBoard/CardBoard";
+import Player from "./Components/Player/Player";
+import NewPlayer from "./Components/NewPlayer/NewPlayer";
+import EndModal from "./Components/EndModal/EndModal";
 
-  import './App.css';
+import "./App.css";
 
-  function App() {
+function App() {
+  const initialCards = [
+    { id: 1, photo: Rocky, flipped: false },
+    { id: 2, photo: Chase, flipped: false },
+    { id: 3, photo: Marshall, flipped: false },
+    { id: 4, photo: Rubble, flipped: false },
+    { id: 5, photo: Skye, flipped: false },
+    { id: 6, photo: Zuma, flipped: false },
+    { id: 7, photo: Rocky, flipped: false },
+    { id: 8, photo: Chase, flipped: false },
+    { id: 9, photo: Marshall, flipped: false },
+    { id: 10, photo: Rubble, flipped: false },
+    { id: 11, photo: Skye, flipped: false },
+    { id: 12, photo: Zuma, flipped: false },
+  ];
 
-    const initialCards = [
-      {id: 1, photo: Rocky, flipped: false},
-      {id: 2, photo: Chase, flipped: false},
-      {id: 3, photo: Marshall, flipped: false},
-      {id: 4, photo: Rubble, flipped: false},
-      {id: 5, photo: Skye, flipped: false},
-      {id: 6, photo: Zuma, flipped: false},
-      {id: 7, photo: Rocky, flipped: false},
-      {id: 8, photo: Chase, flipped: false},
-      {id: 9, photo: Marshall, flipped: false},
-      {id: 10, photo: Rubble, flipped: false},
-      {id: 11, photo: Skye, flipped: false},
-      {id: 12, photo: Zuma, flipped: false},
-    ];
+  const [cards, setCards] = useState(
+    initialCards.sort(() => Math.random() - 0.4)
+  );
+  const [frontCard, setFrontCard] = useState([]);
+  const [cardFound, setCardFound] = useState([]);
+  const [finalCards, setFinalCards] = useState([]);
+  const [moves, setMoves] = useState(0);
+  const [time, setTime] = useState(0);
+  const [timeOn, setTimeOn] = useState(false);
+  const [startModal, setStartModal] = useState(true);
+  const [playerName, setPlayerName] = useState("");
+  const [endGame, setEndGame] = useState(false);
+  const [reset, setReset] = useState(false);
 
-    const [cards, setCards] = useState(initialCards.sort(() => Math.random() - 0.4));
-    const [frontCard, setFrontCard] = useState([]);
-    const [cardFound, setCardFound] = useState([]);
-    const [finalCards, setFinalCards] = useState([]);
-    const [moves, setMoves] = useState(0);
-    const [ time, setTime ] = useState(0);
-    const [ timeOn, setTimeOn ] = useState(false);
-    const [startModal, setStartModal] = useState(true);
-    const [playerName, setPlayerName] = useState("");
-    const [endGame, setEndGame] = useState(false);
-    const [reset, setReset] = useState(false);
+  const startDiv = (
+    <NewPlayer
+      startModal={startModal}
+      setStartModal={setStartModal}
+      playerName={playerName}
+      setPlayerName={setPlayerName}
+    />
+  );
 
-    const startDiv = (
-      <NewPlayer 
-        startModal={startModal}
-        setStartModal={setStartModal}
-        playerName={playerName}
-        setPlayerName={setPlayerName}/>
-    );
+  const endModal = (
+    <EndModal
+      moves={moves}
+      time={time}
+      endGame={endGame}
+      setEndGame={setEndGame}
+      setStartModal={setStartModal}
+      playerName={playerName}
+      setReset={setReset}
+    />
+  );
 
-    const endModal = (
-      <EndModal 
-        moves={moves}
+  const gameBoard = (
+    <div className={endGame ? "app end-game" : "app"}>
+      <Player
         time={time}
-        endGame={endGame}
-        setEndGame={setEndGame}
-        setStartModal={setStartModal}
+        setTime={setTime}
+        timeOn={timeOn}
+        setTimeOn={setTimeOn}
+        frontCard={frontCard}
         playerName={playerName}
-        setReset={setReset}
-        />
-    );
-
-    const gameBoard = (
-      <div className={endGame ? "app end-game" : "app"}>
-        <Player
-          time={time}
-          setTime={setTime}
+        setPlayerName={setPlayerName}
+        moves={moves}
+      />
+      <div className='card-grid'>
+        <CardBoard
+          cards={cards}
+          setCards={setCards}
+          frontCard={frontCard}
+          setFrontCard={setFrontCard}
+          cardFound={cardFound}
+          initialCards={initialCards}
+          setCardFound={setCardFound}
+          finalCards={finalCards}
+          setFinalCards={setFinalCards}
           timeOn={timeOn}
           setTimeOn={setTimeOn}
-          frontCard={frontCard}
-          playerName={playerName}
-          setPlayerName={setPlayerName}
-          moves={moves}/>
-          <div className="card-grid">
-            <CardBoard
-              cards={cards}
-              setCards={setCards}
-              frontCard={frontCard}
-              setFrontCard={setFrontCard}
-              cardFound={cardFound}
-              initialCards={initialCards}
-              setCardFound={setCardFound}
-              finalCards={finalCards}
-              setFinalCards={setFinalCards}
-              timeOn={timeOn}
-              setTimeOn={setTimeOn}
-              moves={moves}
-              setMoves={setMoves}
-              setEndGame={setEndGame}/>
-          </div>
+          moves={moves}
+          setMoves={setMoves}
+          setEndGame={setEndGame}
+        />
       </div>
-    );
-  
-    return (
-      <div>
-        {endGame ? endModal : null}
-        {startModal ? gameBoard : startDiv}
-      </div>
-    );
-  }
+    </div>
+  );
 
-  export default App;
+  return (
+    <div>
+      {endGame ? endModal : null}
+      {startModal ? gameBoard : startDiv}
+    </div>
+  );
+}
+
+export default App;
